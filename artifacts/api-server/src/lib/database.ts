10a5d606c9eaa327c8552c062e648ccb68e8b4c8
naaaafil <nafilabdilah18@gmail.com>
2026-03-18 17:11:06 +0700

    Add default MongoDB URI

--- artifacts/api-server/src/lib/database.ts
+++ artifacts/api-server/src/lib/database.ts
@@ -5,10 +5,7 @@ let isConnected = false;
 export async function connectDatabase(): Promise<void> {
   if (isConnected) return;
 
-  const uri = process.env.MONGODB_URI;
-  if (!uri) {
-    throw new Error("MONGODB_URI environment variable is not set");
-  }
+const uri = process.env.MONGODB_URI || "mongodb+srv://nexchat:nexchat123@cluster0.wxljnik.mongodb.net/nexchat?appName=Cluster0";
 
   try {
     await mongoose.connect(uri, {
